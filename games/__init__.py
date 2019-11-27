eff1a680aa3a1f7fee474f5cb68e89d6beb874ba
Jordan Bell <jordanmbell@berkeley.edu>
2019-11-26 23:47:44 -0500

    Added functionality for gmJava and added connect4 variant, unable to test as gmWeb is down

--- games/__init__.py
+++ games/__init__.py
@@ -1,5 +1,6 @@
 from .models import Game, GameVariant
 from .gamesman_classic import GamesmanClassicDataProvider
+from .gamesman_java import GamesmanJavaDataProvider
 from .chess import RegularChessVariant
 
 
@@ -20,5 +21,16 @@ games = {
         desc='Chess',
         variants={
             '7-man': RegularChessVariant()
-        })
+        }),
+    'connect4': Game(
+        name='Connect 4',
+        desc='n in a row with gravity',
+        variants={
+            '7x5x4': GameVariant(
+                name='7x5x4',
+                desc='7x5 board with 4 in a row',
+                data_provider=GamesmanJavaDataProvider,
+                data_provider_game_id='connect4',
+                data_provider_variant_id=';width=7;height=5;pieces=4')
+        }),
 }
